d5910847d7398dd332c3d8ca669aeb3dd7e2d80a
Jorgeberlin <jorpeberlin@gmail.com>
2026-09-26 00:28:33 +0200

    WIP: ratios

---
+++ quantmgmt/risk/ratios.py
@@ -0,0 +1,71 @@
+import numpy as np
+import pandas as pd
+from typing import Literal
+
+from .returns import to_returns, cagr
+from .volatility import downside_deviation
+from .drawdown import max_drawdown
+
+# asumiendo que la rf es constante, esto podmeos cambiarlo en el futuro!
+def sharpe_ratio(   
+    prices: pd.Series | pd.DataFrame,
+    period: Literal["daily", "monthly"],
+    risk_free_rate: float = 0,
+    method: str = "simple",
+) -> float | pd.Series:
+
+    if period == "daily":
+        periods_per_year = 252
+    elif period == "monthly":
+        periods_per_year = 12
+    else:
+        raise ValueError("period must be either 'daily' or 'monthly'")
+
+    returns = to_returns(prices, method=method)
+
+    risk_free_period = risk_free_rate / periods_per_year
+
+    excess_returns = returns - risk_free_period
+
+    return (
+        excess_returns.mean()
+        / returns.std(ddof=1)
+        * np.sqrt(periods_per_year)
+    )
+
+def sortino_ratio(
+    prices: pd.Series | pd.DataFrame,
+    period: Literal["daily", "monthly"],
+    risk_free_rate: float = 0,
+    method: str = "simple",
+) -> float | pd.Series:
+
+    if period == "daily":
+        periods_per_year = 252
+    elif period == "monthly":
+        periods_per_year = 12
+    else:
+        raise ValueError("period must be either 'daily' or 'monthly'")
+
+    returns = to_returns(prices, method=method)
+
+    annualized_return = returns.mean() * periods_per_year
+    excess_return = annualized_return - risk_free_rate
+
+    downside = downside_deviation(
+        prices,
+        period=period,
+        mar=risk_free_rate / periods_per_year,
+        method=method,
+    )
+
+    return excess_return / downside
+
+def calmar_ratio(
+    prices: pd.Series | pd.DataFrame,
+) -> float | pd.Series:
+
+    annualized_return = cagr(prices)
+    drawdown = max_drawdown(prices)
+
+    return annualized_return / abs(drawdown)
